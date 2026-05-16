828a6ab050a9b71cbfe38fb8b806ba338e281b80
Spas Zahariev <spaszahariev54@gmail.com>
2026-05-16 20:37:57 +0200

    fixed the toast to hober on top of the rest of the ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     'text-6xl',
     'py-6',
     'font-pixel',
+    'fixed',
+    'top-0',
+    'left-0',
+    'right-0',
+    'z-50',
+    'pointer-events-none',
+    'pointer-events-auto',
   ],
   theme: {
     extend: {
